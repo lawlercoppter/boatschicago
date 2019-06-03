d41c320677a415f223b755e29966cdd535fc19b1
T. Russell Schwartz <tschwartz555@gmail.com>
2019-06-03 11:51:23 -0500

    caorusel working properly, need style full screen

---
+++ app.js
@@ -0,0 +1,11 @@
+const full_button = document.getElementsByClassName("flickity-button-icon");
+const carousel_img = document.getElementsByClassName("cell-image");
+
+full_button.addEventListener("click", () => {
+  fullScreenImage();
+});
+
+function fullScreenImage() {
+  carousel_img.setAttribute("style", "max-width: 900px");
+  console.log("the potato is in");
+}
