adc2972e2826955c495bd9bd73c620a60f68c4ff
T. Russell Schwartz <tschwartz555@gmail.com>
2019-06-27 12:51:05 -0500

    carousel and lightbox working.

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const full_button = document.getElementsByClassName("flickity-button-icon");
-const carousel_img = document.getElementsByClassName("cell-image");
-
-full_button.addEventListener("click", function() {
-  fullScreenImage();
-});
-
-function fullScreenImage() {
-  carousel_img.setAttribute("style", "max-width: 900px");
-  console.log("the potato is in");
-}
+//JS for light box/carousel
